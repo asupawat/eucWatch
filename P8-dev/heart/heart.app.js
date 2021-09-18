e1e5dd971a5a4c0feee667ddf531a03f70764ed9
Noom <asupawat@users.noreply.github.com>
2021-09-18 18:51:17 +0700

    Update heart.app.js

--- P8-dev/heart/heart.app.js
+++ P8-dev/heart/heart.app.js
@@ -360,14 +360,83 @@ function stopMeasure() {
   }
 }
 
-TC.on("swipe",(dir)=>{
-  if (dir==TC.RIGHT) startMeasure();
-  else if (dir==TC.LEFT) stopMeasure();
-});
 
-E.on("kill",()=>{stopMeasure();});
+face[0] = { //the first face of the hello app, called by using `face.go("hello",0)` from the cli.
+  offms: 10000, //face timeout, will fall to face[1] after it, face[1] is a redirection face, not actually visible.
+  init: function(o){ //put here the elements of the page that will not need refreshing and initializations.
 
-setTimeout(()=>{
-   E.showMessage("Swipe right\n to start.\n Swipe left\n to stop.","Heart Rate");
-},500);
-
+    this.btn=0;
+    this.last_btn=this.btn;
+	this.run=true;
+  },
+  show : function(o){
+    if (!this.run) return;
+    if (this.btn!==this.last_btn){
+      this.last_btn=this.btn;      
+    }
+    this.tid=setTimeout(function(t){ //the face's screen refresh rate. 
+      t.tid=-1;
+      //doread();
+    },40,this);
+  },
+  tid:-1,
+  run:false,
+  clear : function(){ //enter here everything needed to clear all app running function on face exit. 
+    g.clear(); //as above
+    this.run=false;
+    if (this.tid>=0) clearTimeout(this.tid); //clears main face[0] timeout loop.
+    this.tid=-1;
+    return true;
+  },
+  off: function(){
+    P8.sleep();
+    this.clear();
+  }
+};
+//Redirection face, is used when time expires or the side button is pressed on page[0].
+face[1] = {
+  offms:1000,
+  init: function(){
+  return true;
+  },//only use this part of the face to set redirection.
+  show : function(){
+   	face.go(face.appRoot[0],face.appRoot[1]); //go to the previous face on screen of the previous app.  
+	//face.go(face.appPrev,face.pagePrev); //go to the previous face on screen, even if it was on the same app. 
+  	//face.go("hello",-1); //sleep and set this face as the on_wake face. 
+	//face.go("main",-1);//sleep and set this face as the on_wake face. 
+	//face.go("main",0);//go to main Clock face. 
+    return true;
+  },
+   clear: function(){
+   return true;
+  },
+   off: function(){
+   this.clear();
+  }
+};	
+//touch actions are set here, e is the event, x,y are the coordinates on screen.
+touchHandler[0]=function(e,x,y){ 
+  switch (e) {
+  case 5: //tap event
+    digitalPulse(D16,1,40); //send short buzz pulse to indicate tap was not acknowledged.
+    break;
+  case 1: //slide down event-on directional swipes the x,y indicate the point of starting the swipe, so one can swipe up/dn on buttons like on the brightenss button at the main settings face. 
+    face.go(face.appPrev,face.pagePrev);return; //return when changing faces, so that this action will not reset this face timeout. 
+    //break;
+  case 2: //slide up event
+    face.go(face.appPrev,face.pagePrev);return;
+    break;
+  case 3: //slide left event
+    stopMeasure();
+    digitalPulse(D16,1,40);    
+    break;
+  case 4: //slide right event
+    startMeasure();
+    //break;
+  case 12: //touch and hold(long press) event
+    digitalPulse(D16,1,40);  
+    break;
+  default: //reset face timeout on every touch action, this function is in the handler file. 
+    this.timeout();
+  }
+};
