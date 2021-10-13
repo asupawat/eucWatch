c803422d3dcc5026bba6f0b6f258dfdcaea9c514
Noom <asupawat@users.noreply.github.com>
2021-10-13 14:41:28 +0700

    Update handler.js

--- P8-testing/handler/handler.js
+++ P8-testing/handler/handler.js
@@ -346,18 +346,23 @@ if ( set.def.touchtype == "0" ) {
 }
 
 if (set.def.touchtype=="816"){ //816
+  var lt;
 	setWatch(function(s){
 		i2c.writeTo(0x15,0);
 		var tp=i2c.readFrom(0x15,7);
 		//print("touch816 :",tp);
 		if (face.pageCurr>=0) {
-			if (tp[1]== 0 && tp[3]==64) {tp[1]=5; set.def.rstR=0xE5;}
+			if (tp[1]== 0 && tp[3]==64) {tp[1]=5; set.def.rstR=0xE5;lt=1;return;}
+      if(tp[1]== 12) {
+        if(lt==0) return;
+        else lt=0;
+      }
 			if (set.def.rstR==0xE5 && tp[1]== 12 ) tp[6]=tp[6]+25;
-			touchHandler[face.pageCurr](tp[1],tp[4],tp[6]);}
-		else if (tp[1]==1) {
+      touchHandler[face.pageCurr](tp[1],tp[4],tp[6]);
+    }else if (tp[1]==1) {
 			face.go(face.appCurr,0);
 		}
-	},D28,{repeat:true, edge:"rising"}); 
+	},D28,{repeat:true, edge:"falling"}); 
 }else if (set.def.touchtype=="816s"){//816s
 	var lt,xt,yt,tt,tf,c;
 	//var ct=0;
@@ -666,4 +671,3 @@ if (!Boolean(require("Storage").read("colmode16"))){
 	}
 }
 //end
-
